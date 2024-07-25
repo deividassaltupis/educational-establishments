a12b24ad445c32e1f8a75a5c06351a0cf7206f34
Deividas <deividas.saltupis@mygom.tech>
2024-07-26 01:53:49 +0300

    feat(setup): configure rollup

---
+++ rollup.config.mjs
@@ -0,0 +1,59 @@
+import nodeResolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import babel from "@rollup/plugin-babel";
+import replace from "@rollup/plugin-replace";
+import typescript from "@rollup/plugin-typescript";
+import serve from "rollup-plugin-serve";
+import livereload from "rollup-plugin-livereload";
+import terser from "@rollup/plugin-terser";
+import copy from "rollup-plugin-copy";
+
+const isProduction = process.env.NODE_ENV === "production";
+
+export default {
+  input: "src/index.tsx",
+  output: {
+    file: "dist/bundle.js",
+    format: "iife",
+    sourcemap: !isProduction,
+  },
+  plugins: [
+    nodeResolve({
+      extensions: [".js", ".jsx", ".ts", ".tsx"],
+    }),
+    typescript({
+      tsconfig: "./tsconfig.json",
+    }),
+    babel({
+      babelHelpers: "bundled",
+      presets: ["@babel/preset-react"],
+      extensions: [".js", ".jsx", ".ts", ".tsx"],
+      compact: false,
+    }),
+    commonjs(),
+    replace({
+      preventAssignment: false,
+      "process.env.NODE_ENV": JSON.stringify(
+        process.env.NODE_ENV || "development"
+      ),
+    }),
+    !isProduction &&
+      serve({
+        open: true,
+        contentBase: "dist",
+        port: 3000,
+      }),
+    !isProduction &&
+      livereload({
+        watch: "dist",
+      }),
+    isProduction && terser(), // Minify code for production
+    isProduction &&
+      copy({
+        targets: [
+          { src: "public/**/*", dest: "dist" },
+          { src: "public/index.html", dest: "dist" },
+        ],
+      }),
+  ].filter(Boolean),
+};
---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const App: React.FC = () => {
+  return (
+    <>
+      <header>
+        <h1>Educational Establishments</h1>
+      </header>
+      <main>
+        <h1>Hello, React with ESBuild and TypeScript!</h1>
+      </main>
+      <footer>Author: Deividas Šaltupis</footer>
+    </>
+  );
+};
+
+export default App;
