45fcf5466bfa06fc8b59602822126fbcc579cdb4
Deividas <deividas.saltupis@mygom.tech>
2024-07-29 02:07:34 +0300

    feat(home): connect pagination state with URL search params

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,6 +1,7 @@
-import { Box, Container, Table } from "@mui/material"
-import React, { useState } from "react"
-import { Link } from "react-router-dom"
+import { Box, Container } from "@mui/material"
+import { useEffect, useState } from "react"
+
+import { Link, useSearchParams } from "react-router-dom"
 import EstablishmentsTable from "src/components/routes/home/establishments-table"
 import { PROXY_SERVER_ENDPOINT_URL } from "src/constants/proxy-sever"
 import useFetch from "src/hooks/useFetch"
@@ -8,9 +9,14 @@ import usePaginatedFetch from "src/hooks/usePaginatedFetch"
 import { EducationalEstablishment } from "src/types/entities/educational-establishment"
 
 const Home = () => {
+  const [searchParams, setSearchParams] = useSearchParams()
+
+  const initialPage = parseInt(searchParams.get("page") || "1", 10)
+  const initialSize = parseInt(searchParams.get("size") || "40", 10)
+
   const [paginationOptions, setPaginationOptions] = useState({
-    page: 1,
-    size: 40
+    page: initialPage,
+    size: initialSize
   })
 
   const { paginatedResponse, isLoading, error } =
@@ -20,7 +26,12 @@ const Home = () => {
       size: paginationOptions.size
     })
 
-  console.log(paginatedResponse?.data)
+  useEffect(() => {
+    setSearchParams({
+      page: paginationOptions.page.toString(),
+      size: paginationOptions.size.toString()
+    })
+  }, [paginationOptions, setSearchParams])
 
   return (
     <Box component="main">
